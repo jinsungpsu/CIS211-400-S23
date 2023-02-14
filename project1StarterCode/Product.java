227e612d4309f0a94109e43f58422b0af53447f4
Jin An <jin.an@dtcc.edu>
2023-02-14 11:25:04 -0500

    Create Product.java

---
+++ project1StarterCode/Product.java
@@ -0,0 +1,38 @@
+/*
+ * @author Delaware Technical Community College
+ * Starter and/or reference code provided for Delaware Technical Community College courses.
+ */
+
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
